b889104f0560da1b600725e1fa3adef91811136f
Thomas Weber <thomas.weber@stud.tu-darmstadt.de>
2015-11-18 13:34:33 +0100

    changed from mysql to mysqli

--- backend/DatabaseConnection.class.php
+++ backend/DatabaseConnection.class.php
@@ -1,57 +1,57 @@
-<?php
+<?
 
 class DatabaseConnection extends DatabaseConfig    {
-  private $connection;
-  private $dbLink;
+private $connection;
+private $dbLink;
 
-  public function __construct($host,$user,$pass,$database)
+public function __construct($host,$user,$pass,$database)
+{
+  parent :: __construct();
+  $this->$hostName = $host;
+  $this->$userName = $user;
+  $this->$password = $pass;
+  $this->$dbName = $database;
+
+  $this->$connection = mysqli_connect($this->$hostName, $this->$userName, $this->$password);
+
+  if(!$this->$connection)
   {
-    parent :: __construct();
-    $this->$hostName = $host;
-    $this->$userName = $user;
-    $this->$password = $pass;
-    $this->$dbName = $database;
-
-    $this->$connection = mysql_connect($this->$hostName, $this->$userName, $this->$password);
-
-    if(!$this->$connection)
-    {
-        die("Can't connect to mysql server: ".mysql_error());
-    }
-
-    $dbLink = mysql_select_db($this->$dbName, $this->$connection);
-
-    if(!$this->$dbLink)
-    {
-        die("Can't connect to database: ".mysql_error());
-    }
+    die("Can't connect to mysql server: ".mysqli_error());
   }
 
-  function close()
+  $dbLink = mysqli_select_db($this->$dbName, $this->$connection);
+
+  if(!$this->$dbLink)
   {
-    mysql_close($this->$connection);
-    $this->$hostName = "";
-    $this->$userName = "";
-    $this->$password = "";
-    $this->$dbName = "";
-    $this->$dbLink = "";
+    die("Can't connect to database: ".mysqli_error());
   }
+}
 
-    function set($query)
+function close()
+{
+  mysqli_close($this->$connection);
+  $this->$hostName = "";
+  $this->$userName = "";
+  $this->$password = "";
+  $this->$dbName = "";
+  $this->$dbLink = "";
+}
+
+function set($query)
+{
+  mysqli_query($query,$this->$dbLink);
+}
+
+function get($query)
+{
+  $get_result = mysqli_query($query);
+  $get_data = array();
+
+  while ($get_row = mysqli_fetch_array($result, MYSQLI_BOTH)) {
     {
-      mysql_query($query,$this->$dbLink);
+      array_push($get_data,$get_row);
     }
-
-    function get($query)
-    {
-      $get_result = mysql_query($query);
-      $get_data = array();
-
-      while ($get_row = mysql_fetch_array($result, MYSQL_BOTH)) {
-      {
-        array_push($get_data,$get_row);
-      }
-      return $get_data;
-    }
+    return $get_data;
+  }
 }
 ?>
